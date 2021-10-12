f31ab777c9a5b13e596be7d99ce6b312e0e80d16
Hawkx1 <gregg.friedman@revature.net>
2021-10-12 16:44:28 -0400

    Completed basic Login/Register Servlet and Login/Register html pages

--- src/main/java/servlets/RegisterServlet.java
+++ src/main/java/servlets/RegisterServlet.java
@@ -4,11 +4,13 @@ import models.UserModel;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class RegisterServlet extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String user = req.getParameter("user_name");
         String pass = req.getParameter("password");
         String fName = req.getParameter("first_name");
@@ -16,7 +18,8 @@ public class RegisterServlet extends HttpServlet {
 
         UserModel newUser = new UserModel(user, pass, fName, lName, false);
 
-        System.out.println(newUser.getUserName() + ", " + newUser.getPass() + ", " + newUser.getFirstName() + ", " +
+        PrintWriter out = resp.getWriter();
+        out.println(newUser.getUserName() + ", " + newUser.getPass() + ", " + newUser.getFirstName() + ", " +
                 newUser.getLastName() + ", " + newUser.isRole());
 
 
