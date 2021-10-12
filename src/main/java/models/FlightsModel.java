2910c9dfe3deca38ecce508f1e1d330caf48b40f
Hawkx1 <gregg.friedman@revature.net>
2021-10-12 01:44:25 -0400

    User and Flight Models completed, attempting to display UserModel in RegisterServlet

--- src/main/java/models/FlightsModel.java
+++ src/main/java/models/FlightsModel.java
@@ -1,4 +1,56 @@
 package models;
 
 public class FlightsModel {
+    private String origin;
+    private String destination;
+    private int flightNumber;
+    private double eta;
+    private boolean checkIn;
+
+    public FlightsModel(String origin, String destination, int flightNumber, double eta) {
+        this.origin = origin;
+        this.destination = destination;
+        this.flightNumber = flightNumber;
+        this.eta = eta;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public int getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(int flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public double getEta() {
+        return eta;
+    }
+
+    public void setEta(double eta) {
+        this.eta = eta;
+    }
+
+    public boolean isCheckIn() {
+        return checkIn;
+    }
+
+    public void setCheckIn(boolean checkIn) {
+        this.checkIn = checkIn;
+    }
 }
--- src/main/java/servlets/RegisterServlet.java
+++ src/main/java/servlets/RegisterServlet.java
@@ -1,4 +1,24 @@
 package servlets;
 
-public class RegisterServlet {
+import models.UserModel;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RegisterServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+        String user = req.getParameter("user_name");
+        String pass = req.getParameter("password");
+        String fName = req.getParameter("first_name");
+        String lName = req.getParameter("last_name");
+
+        UserModel newUser = new UserModel(user, pass, fName, lName, false);
+
+        System.out.println(newUser.getUserName() + ", " + newUser.getPass() + ", " + newUser.getFirstName() + ", " +
+                newUser.getLastName() + ", " + newUser.isRole());
+
+
+    }
 }
